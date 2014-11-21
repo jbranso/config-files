f088acd521b16061a587be5ce5437b7155011c0a
Joshua Branson <jbranso@purdue.edu>
2014-11-21 11:38:10 -0500

    I have added more stuff.

---
+++ programming/html/purdue_site/site1/skills.php
@@ -0,0 +1,33 @@
+<?php include "_template-top.html"; ?>
+
+<div class="container">
+  <br/><br/>
+
+  <div class="jumbotron">
+    <div class="row">
+      <a href="http://www.w3schools.com/default.asp" target="_blank">
+	<img src="./images/html.png" width="199px;" hight="122px;" class="right" alt="GNU" id="skills_img"></a>
+	<p>
+	  Working knowledge of HTML, CSS, jQuery, javascript, php, and MySQL. I did make this site,
+	  which uses twitter's <a href="http://getbootstrap.com">bootstrap</a>.<br><br>
+	  Operate and run Arch Linux on a Macbook,
+	  from which I have acquired a good working knowledge of UNIX commands including chmod, chown,
+	  chgrp, ls, cd, cp, mv, etc.<br><br>
+	  Can partition hard drives and create ext2, ext3, ext4, btrfs, and
+	  more filesystems.<br><br>
+	  Can configure the bootloader grub2 to decrease boot time and change the linux kernel boot options.<br><br>
+	  Can configure initial ram disk environment to load specific kernel
+	  modules early in the boot process including nouveau, ext4, and
+	  others.
+	</p>
+    </div>
+
+
+  </div>
+</div>
+</div>
+
+</div><!-- /.container -->
+<br/><br/>
+
+<?php include "_template-bottom.html"; ?>
