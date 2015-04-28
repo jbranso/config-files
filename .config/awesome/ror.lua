ce6c9bd62d0058cf86208860574066bbd17b36fd
Joshua Branson <jbranso@purdue.edu>
2015-04-28 10:03:26 -0400

    I have made more gnus changes.c

--- .config/awesome/ror.lua
+++ .config/awesome/ror.lua
@@ -22,7 +22,7 @@ table5={
    -- Mod-C-n is for unminimizing applications.
    --   ["Control-n"]={"firefox-nightly -P nightly", "Firefox Nightly"},
   --   ["Control-f"]={"firefox -P default","Firefox"},
-   ["Control-h"]={"nice -n -5 chromium","Chromium"},
+   ["Control-h"]={"chromium","Chromium"},
    -- I don't use gimp often enough, and it gets in the way of mod C-c
    --["Control-g"]={"gimp-2.8", "Gimp-2.8"},
    ["Control-t"]={"thunderbird", "Thunderbird"},
