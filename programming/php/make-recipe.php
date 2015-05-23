4bbba847cdd0e758cf78c08c283d7a3e75e7d94e
Joshua Branson <jbranso@purdue.edu>
2015-05-23 16:25:38 -0400

    I have made some changes.

--- programming/php/make-recipe.php
+++ programming/php/make-recipe.php
@@ -2,25 +2,22 @@
 <?php
 ini_set('display_errors', 'On');
 error_reporting(E_ALL | E_STRICT);
+$errorReporting = false;
 ?>
 <html lang="en">
-    <head> -->
-        <meta charset="utf-8"> -->
+    <head>
+        <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Bootstrap 101 Template</title>
+        <title>Make A Recipe</title>
 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-
-
-        <!-- <\\!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -\\-> -->
-        <!-- <\\!-- WARNING: Respond.js doesn't work if you view the page via file:// -\\-> -->
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
     </head>
     <body>
         <div class="container">
 
-            <h1>Build Your Recipe snth</h1>
+            <h1>Build Your Recipe</h1>
 
             <h3>Enter a food item</h3>
             <form class="form-horizontal" action="make-recipe.php" method="get" id="foods">
@@ -51,118 +48,141 @@ error_reporting(E_ALL | E_STRICT);
                     </div>
                 </div>
             </form>
-        </div>
 
-        <?php
+            <?php
 
+            echo strtolower(htmlspecialchars($_GET['food1']))." ".
+                 strtolower(htmlspecialchars($_GET['food2']))." ".
+                 strtolower(htmlspecialchars($_GET['food3']))." <br/>";
 
-        //set up a connection to the database.
-
-        //create a user that is 6 foot 138 lbs. (it is assumed that he works out lightly 1-3 hours per week.
-        //He needs 21g P, 64g C, 9g Fat per meal if I eat 6 meals a day.
-        //That is 126g P, 384g C, 54g F
-        include "connect-to-database.php";
-
-        function buildQuery ($query) {
-            //do some stuff
-            return $sql;
-        }
-        //These arrays will store the results of the queries
-        $protein = array();
-        $carbs = array();
-        $fat = array();
-        $name = array();
-
-        $res = $mysqli->query("SELECT * FROM food WHERE name='milk powder' OR name='oatmeal' OR name='peanut butter' ORDER BY NAME ASC");
-        # This will be the two dimensional array, that will store the result of the query.
-        while ($row = $res->fetch_assoc()) {
-            $protein[] = $row['protein'];
-            $carbs[] = $row['carbs'];
-            $fat[] = $row['fat'];
-            $name[] = $row['name'];
-        }
-        echo "<br>";
-
-        //echo print_r($protein)."<br>";
-        //echo print_r($carbs)."<br>";
-        //echo print_r($fat)."<br>";
-
-        //echo "The number of column in the table food is ".$mysqli->field_count."<br>";
-
-        //Php has something called prepared statements. This lets you execute MANY similiar queries at a very high rate.
-        //Basically you tell the database that you know most of the query, but at a later time you will give it certain values, like
-        //INSERT INTO PRODUCT (name, price) VALUES (?, ?)
-        //http://stackoverflow.com/questions/60174/how-can-i-prevent-sql-injection-in-php
-
-        # out put the results.  What the user should eat.
-        function results ($array) {
-            #My regexp splits the string but in a weird way: the first and last element of the array is null. So:
-            #array[0] == null
-            #array[1] == a number
-            #array[2] == a number
-            #array[3] == a number
-            #array[4] == null
-            global $name;
-            $itemsInArray = count($array) - 2;
-            echo "<br>";
-            $j = 0;
-            for ($i = 1; $i <= $itemsInArray; $i++ ) {
-                echo "<br>";
-                echo $array[$i]." servings of $name[$j]";
-                $j++;
+            $foods = array();
+            if (isset($_GET['food1'])) {
+                $foods[] = strtolower($_GET['food1']);
             }
-        }
 
-        //build the string to send into the shell. It will be of the form:
-        // octave --eval 'a = [ 4 3 4; 3 2 4; 2 4 3]; b = [4; 4; 3]; a \ b' | egrep blah blah
-        $octaveString = "octave --eval 'a = [";
+            if (isset($_GET['food2'])) {
+                $foods[] = strtolower($_GET['food2']);
+            }
 
-        for ($i = 0; $i < 3; $i++) {
-            $octaveString .= $protein[$i]." ";
-        }
-        //$protein[0]." ".$protein[1]." ".$protein[2].
-        $octaveString = $octaveString.";";
-        for ($i = 0; $i < 3; $i++) {
-            $octaveString .= $carbs[$i]." ";
-        }
-        $octaveString = $octaveString.";";
-        for ($i = 0; $i < 3; $i++) {
-            $octaveString .= $fat[$i]." ";
-        }
-        //$carbs[0]." ".$carbs[1]." ".$carbs[2].";".
-        //$fat[0]." ".$fat[1]." ".$fat[2]."]; ".
-        $octaveString .= "]; b = [21; 64; 9]; a \ b' | egrep '[-]*[0-9]+\.[0-9]{3,}$' ";
-        echo $octaveString."<br>";
+            if (isset($_GET['food3'])) {
+                $foods[] = strtolower($_GET['food3']);
+            }
 
-        //$result=shell_exec("octave --eval 'a = [1 3 6; 4 6 3; 4 6 9]; b = [3; 6; 9]; a \ b' | egrep '[-]*[0-9]+\.[0-9]{3,}$' ");
-        $result = shell_exec($octaveString);
+            if ($errorReporting ) {
+                print_r($foods);
+            }
 
-        $aString = preg_split("/[\s,]+/", $result);
+            //set up a connection to the database.
 
-        print_r($aString);
-        echo "<br>";
+            //create a user that is 6 foot 138 lbs. (it is assumed that he works out lightly 1-3 hours per week.
+            //He needs 21g P, 64g C, 9g Fat per meal if I eat 6 meals a day.
+            //That is 126g P, 384g C, 54g F
+            include "connect-to-database.php";
 
-        results ($aString);
+            function buildQuery ($query) {
+                //do some stuff
+                return $sql;
+            }
+            //These arrays will store the results of the queries
+            $protein = array();
+            $carbs = array();
+            $fat = array();
+            $name = array();
 
-        //Close the connection
-        $mysqli->close();
+            echo "<br> ".$foods[0]." <br>";
 
-        //print_r($result);
-        // <\!-- include "connect-to-database.php"; -\->
+            $sql = "SELECT * FROM food WHERE name='".$foods[0]."'".
+                   " OR name='pumpkin'".
+                   " OR name='peanut butter'".
+                   " ORDER BY NAME ASC";
 
-        // <\!-- $res = $mysqli->query("SELECT 'choices to please everybody.' AS _msg FROM DUAL"); -\->
-        // <\!-- $row = $res->fetch_assoc(); -\->
-        // <\!-- echo $row['_msg']; -\->
+            echo "<br>".$sql." <br>";
 
-        ?>
+            $res = $mysqli->query($sql);
+            # This will be the two dimensional array, that will store the result of the query.
+            while ($row = $res->fetch_assoc()) {
+                $protein[] = $row['protein'];
+                $carbs[] = $row['carbs'];
+                $fat[] = $row['fat'];
+                $name[] = $row['name'];
+            }
+            echo "<br>";
+
+            if ($errorReporting) {
+                echo print_r($protein)."<br>";
+                echo print_r($carbs)."<br>";
+                echo print_r($fat)."<br>";
+                echo "The number of column in the table food is ".$mysqli->field_count."<br>";
+            }
+
+            //Php has something called prepared statements. This lets you execute MANY similiar queries at a very high rate.
+            //Basically you tell the database that you know most of the query, but at a later time you will give it certain values, like
+            //INSERT INTO PRODUCT (name, price) VALUES (?, ?)
+            //http://stackoverflow.com/questions/60174/how-can-i-prevent-sql-injection-in-php
+
+            # out put the results.  What the user should eat.
+            function results ($array) {
+                #My regexp splits the string but in a weird way: the first and last element of the array is null. So:
+                #array[0] == null
+                #array[1] == a number
+                #array[2] == a number
+                #array[3] == a number
+                #array[4] == null
+                global $name;
+                $itemsInArray = count($array) - 2;
+                echo "<br>";
+                $j = 0;
+                for ($i = 1; $i <= $itemsInArray; $i++ ) {
+                    echo "<br>";
+                    echo $array[$i]." servings of $name[$j]";
+                    $j++;
+                }
+            }
+
+            //build the string to send into the shell. It will be of the form:
+            // octave --eval 'a = [ 4 3 4; 3 2 4; 2 4 3]; b = [4; 4; 3]; a \ b' | egrep blah blah
+            $octaveString = "octave --eval 'a = [";
+
+            for ($i = 0; $i < 3; $i++) {
+                $octaveString .= $protein[$i]." ";
+            }
+            //$protein[0]." ".$protein[1]." ".$protein[2].
+            $octaveString = $octaveString.";";
+            for ($i = 0; $i < 3; $i++) {
+                $octaveString .= $carbs[$i]." ";
+            }
+            $octaveString = $octaveString.";";
+            for ($i = 0; $i < 3; $i++) {
+                $octaveString .= $fat[$i]." ";
+            }
+            //$carbs[0]." ".$carbs[1]." ".$carbs[2].";".
+            //$fat[0]." ".$fat[1]." ".$fat[2]."]; ".
+            $octaveString .= "]; b = [21; 64; 9]; a \ b' | egrep '[-]*[0-9]+\.[0-9]{3,}$' ";
+            echo $octaveString."<br>";
+
+            //$result=shell_exec("octave --eval 'a = [1 3 6; 4 6 3; 4 6 9]; b = [3; 6; 9]; a \ b' | egrep '[-]*[0-9]+\.[0-9]{3,}$' ");
+            $result = shell_exec($octaveString);
+
+            $aString = preg_split("/[\s,]+/", $result);
+
+            print_r($aString);
+            echo "<br>";
+
+            results ($aString);
+
+            //Close the connection
+            $mysqli->close();
+
+            ?>
+        </div>
         <script src="http://cdn.jsdelivr.net/jquery.validation/1.13.1/jquery.validate.min.js"></script>
 
         <script>
          $(document).ready(function(){
-             ///jQuery.validator.addMethod("titleReg", function(value, element, param) {
-             //return value.match(new RegExp("^" + param + "$"));
-             //},
-             //            "Your input cannot have special characters.");
+             jQuery.validator.addMethod("titleReg", function(value, element, param) {
+                 return value.match(new RegExp("^" + param + "$"));
+             },
+                                        "Your input cannot have special characters.");
 
 
 
